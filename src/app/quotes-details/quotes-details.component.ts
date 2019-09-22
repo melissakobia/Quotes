2a67d03011033c0f1a31fe2e4793ea6350aa810d
melissa kobia <melissakobia1997@gmail.com>
2019-09-22 19:04:17 +0300

    Adds a decorator that allows recieving of data from parent

---
+++ src/app/quotes-details/quotes-details.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Quotes } from 'src/app/quotes';
+import { from } from 'rxjs';
+
+@Component({
+  selector: 'app-quotes-details',
+  templateUrl: './quotes-details.component.html',
+  styleUrls: ['./quotes-details.component.css']
+})
+export class QuotesDetailsComponent implements OnInit {
+
+  @Input() quote: Quotes;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
