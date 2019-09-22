f7c04faf3febb620d89fcae8aba8559c8e3a2346
melissa kobia <melissakobia1997@gmail.com>
2019-09-22 23:56:00 +0300

    Adds a delete function

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -11,11 +11,23 @@ import { Quotes } from 'src/app/quotes';
 export class QuotesComponent implements OnInit {
 
   quotes:Quotes [] = [
-   new Quotes ('Hell','I','am happy'),
+   new Quotes (1,'Hell0','I','am happy'),
   ];
+
+  addNewQuote(quote) {
+    const quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    this.quotes.push(quote);
+  }
 
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
+
+  }
+  deleteQuote(isDelete, index){
+    if (isDelete) {
+      this.quotes.splice(index,1);
+    }
   }
 
   constructor() { }
