2797a8284cdb08b5b176c0d6352b3f68b8bba1ce
melissa kobia <melissakobia1997@gmail.com>
2019-09-22 15:41:26 +0300

    Adds an event emitter function

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Quotes } from '../quotes';
 import { from } from 'rxjs';
 
@@ -9,6 +9,12 @@ import { from } from 'rxjs';
 })
 export class QuotesFormComponent implements OnInit {
   newQuote = new Quotes ("", "","");
+
+  @Output() addQuote = new EventEmitter<Quotes>();
+
+  submitGoal(){
+    this.addQuote.emit(this.newQuote);
+  }
 
   constructor() { }
 
